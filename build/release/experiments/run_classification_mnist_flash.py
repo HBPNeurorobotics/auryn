95732fbed95f470eb7c499478a9f8b797f94a2c7
Alexander Friedrich <friedric@fzi.de>
2018-02-04 18:38:12 +0100

    Added: Enter debug mode on exception

--- build/release/experiments/run_classification_mnist_flash.py
+++ build/release/experiments/run_classification_mnist_flash.py
@@ -1,9 +1,12 @@
 import utils.generate_ras as gras
 import os, sys
+import experimentLib as elib
+import traceback, code
+import pdb
 
 context={'ncores':8,
          'directory' : 'dvs_mnist_flash',
-         'nv' : 784, #Include nc
+         'nv' : 128*128+10, #Include nc
          'nh' : 400,
          'nh2' : 200,
          'nh1' : 200,
@@ -33,7 +36,7 @@ context={'ncores':8,
          'max_samples_train' : 60000,
          'max_samples_test' : 10000,
          'n_samples_train' : 10,
-         'n_samples_test' : 10,
+         'n_samples_test' : 10000,
          'n_epochs' : 1, #60
          'n_loop' : 1,
          'prob_syn' : .65,
@@ -52,21 +55,28 @@ context={'ncores':8,
 context['eta_orig'] = context['eta']
 
 if __name__ == '__main__':
-    test_labels_name = context['test_labels_name']
-    train_labels_name = context['train_labels_name']
-    max_samples_train = context['max_samples_train']
-    max_samples_test = context['max_samples_test']
-    n_samples_train = context['n_samples_train']
-    n_samples_test  = context['n_samples_test']
-    n_epochs        = context['n_epochs']
-    test_every      = context['test_every']
-    nv = context['nv']
-    nh = context['nh']
-    nc = context['nc']
+    try:
+        test_labels_name = context['test_labels_name']
+        train_labels_name = context['train_labels_name']
+        max_samples_train = context['max_samples_train']
+        max_samples_test = context['max_samples_test']
+        n_samples_train = context['n_samples_train']
+        n_samples_test  = context['n_samples_test']
+        n_epochs        = context['n_epochs']
+        test_every      = context['test_every']
+        nv = context['nv']
+        nh = context['nh']
+        nc = context['nc']
 
-    os.system('rm -rf inputs/{directory}/train/'.format(**context))
-    os.system('rm -rf inputs/{directory}/test/'.format(**context))
-    os.system('mkdir -p inputs/{directory}/train/' .format(**context))
-    os.system('mkdir -p inputs/{directory}/test/' .format(**context))
+        os.system('rm -rf inputs/{directory}/train/'.format(**context))
+        os.system('rm -rf inputs/{directory}/test/'.format(**context))
+        os.system('mkdir -p inputs/{directory}/train/' .format(**context))
+        os.system('mkdir -p inputs/{directory}/test/' .format(**context))
 
-    gras.create_event_data_rbp(n_samples_test, max_samples_test, "dvs_mnist_flash", "test", test_labels_name, randomize = True)
+        elib.create_rbp_init(base_filename = 'inputs/{directory}/train/fwmat'.format(**context), **context)
+
+        gras.create_event_data_rbp(n_samples_test, max_samples_test, '{directory}'.format(**context), "test", test_labels_name, randomize = False)
+    except:
+        type, value, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
