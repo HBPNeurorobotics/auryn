0db549f790b1f74f5c4e90d496c3bb195efa5b42
Alexander Friedrich <friedric@fzi.de>
2018-02-06 11:47:10 +0100

    Reformatting cpp files

--- experiments/exp_rbp_dual.cpp
+++ experiments/exp_rbp_dual.cpp
@@ -1,31 +1,31 @@
-/* 
+/*
 * Copyright 2014-2016 Friedemann Zenke
 *
 * This file is part of Auryn, a simulation package for plastic
 * spiking neural networks.
-* 
+*
 * Auryn is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
-* 
+*
 * Auryn is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
 * along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*!\file 
+/*!\file
  *
  * \brief Simulation code for the Vogels Abbott benchmark following Brette et al. (2007)
  *
  * This simulation implements the Vogels Abbott benchmark as suggested by
- * Brette et al. (2007) Journal of Computational Neuroscience 23: 349-398. 
+ * Brette et al. (2007) Journal of Computational Neuroscience 23: 349-398.
  *
- * The network is based on a network by Vogels and Abbott as described in 
+ * The network is based on a network by Vogels and Abbott as described in
  * Vogels, T.P., and Abbott, L.F. (2005).  Signal propagation and logic gating
  * in networks of integrate-and-fire neurons. J Neurosci 25, 10786.
  *
@@ -46,22 +46,22 @@ using namespace auryn;
 namespace po = boost::program_options;
 
 int main(int ac,char *av[]) {
-  bool record_rates;
-  bool record;
-  bool record_rasters;
-  bool learning;
+    bool record_rates;
+    bool record;
+    bool record_rasters;
+    bool learning;
 
-	string dir = "/tmp";
+    string dir = "/tmp";
 
-	string fwmat_ve = "";
-	string fwmat_vh = "";
-	string fwmat_ho = "";
-	string fwmat_hh = "";
-  string fwmat_oe = "";
-  string fwmat_eo = "";
-  string fwmat_eh = "";
-  string ip_v = "";
-  string ipat_file = "ipat";
+    string fwmat_ve = "";
+    string fwmat_vh = "";
+    string fwmat_ho = "";
+    string fwmat_hh = "";
+    string fwmat_oe = "";
+    string fwmat_eo = "";
+    string fwmat_eh = "";
+    string ip_v = "";
+    string ipat_file = "ipat";
 
 	std::stringstream oss;
 	string strbuf ;
@@ -71,10 +71,10 @@ int main(int ac,char *av[]) {
 	double simtime = 20.;
 	double stimtime = 10.;
 	double eta = 0.;
-  double sigma = 50e-3;
-  double prob_syn = 1.0;
-  double gate_lo = -15.;
-  double gate_hi = 15.;
+    double sigma = 50e-3;
+    double prob_syn = 1.0;
+    double gate_lo = -15.;
+    double gate_hi = 15.;
 
 	NeuronID nout = 10;
 	NeuronID nvis = 794;
@@ -116,39 +116,39 @@ int main(int ac,char *av[]) {
 
         ;
 
-        po::variables_map vm;        
+        po::variables_map vm;
         po::store(po::parse_command_line(ac, av, desc), vm);
-        po::notify(vm);    
+        po::notify(vm);
 
         if (vm.count("help")) {
             std::cout << desc << "\n";
             return 1;
         }
 
-        if (vm.count("simtime")) simtime = vm["simtime"].as<double>(); 
-        if (vm.count("stimtime")) stimtime = vm["stimtime"].as<double>(); 
-        if (vm.count("dir")) dir = vm["dir"].as<string>(); 
-        if (vm.count("fve")) fwmat_ve = vm["fve"].as<string>();
-        if (vm.count("fvh")) fwmat_vh = vm["fvh"].as<string>();
-        if (vm.count("fho")) fwmat_ho = vm["fho"].as<string>();
-        if (vm.count("fhh")) fwmat_hh = vm["fhh"].as<string>();
-        if (vm.count("foe")) fwmat_oe = vm["foe"].as<string>();
-        if (vm.count("feo")) fwmat_eo = vm["feo"].as<string>();
-        if (vm.count("feh")) fwmat_eh = vm["feh"].as<string>();
-        if (vm.count("learn")) learning = vm["learn"].as<bool>();
-        if (vm.count("record_full")) record = vm["record_full"].as<bool>();
-        if (vm.count("record_rates")) record_rates = vm["record_rates"].as<bool>();
+        if (vm.count("simtime"))        simtime = vm["simtime"].as<double>();
+        if (vm.count("stimtime"))       stimtime = vm["stimtime"].as<double>();
+        if (vm.count("dir"))            dir = vm["dir"].as<string>();
+        if (vm.count("fve"))            fwmat_ve = vm["fve"].as<string>();
+        if (vm.count("fvh"))            fwmat_vh = vm["fvh"].as<string>();
+        if (vm.count("fho"))            fwmat_ho = vm["fho"].as<string>();
+        if (vm.count("fhh"))            fwmat_hh = vm["fhh"].as<string>();
+        if (vm.count("foe"))            fwmat_oe = vm["foe"].as<string>();
+        if (vm.count("feo"))            fwmat_eo = vm["feo"].as<string>();
+        if (vm.count("feh"))            fwmat_eh = vm["feh"].as<string>();
+        if (vm.count("learn"))          learning = vm["learn"].as<bool>();
+        if (vm.count("record_full"))    record = vm["record_full"].as<bool>();
+        if (vm.count("record_rates"))   record_rates = vm["record_rates"].as<bool>();
         if (vm.count("record_rasters")) record_rasters = vm["record_rasters"].as<bool>();
-        if (vm.count("eta")) eta = vm["eta"].as<double>();
-        if (vm.count("sigma")) sigma = vm["sigma"].as<double>();
-        if (vm.count("prob_syn")) prob_syn = vm["prob_syn"].as<double>();
-        if (vm.count("gate_low")) gate_lo = vm["gate_low"].as<double>();
-        if (vm.count("gate_high")) gate_hi = vm["gate_high"].as<double>();
-        if (vm.count("nvis")) nvis = vm["nvis"].as<int>();
-        if (vm.count("nhid")) nhid = vm["nhid"].as<int>();
-        if (vm.count("nout")) nout = vm["nout"].as<int>();
-  	    if (vm.count("ip_v")) ip_v  = vm["ip_v"].as<string>();
-  	    if (vm.count("ip_file")) ipat_file  = vm["ip_file"].as<string>();
+        if (vm.count("eta"))            eta = vm["eta"].as<double>();
+        if (vm.count("sigma"))          sigma = vm["sigma"].as<double>();
+        if (vm.count("prob_syn"))       prob_syn = vm["prob_syn"].as<double>();
+        if (vm.count("gate_low"))       gate_lo = vm["gate_low"].as<double>();
+        if (vm.count("gate_high"))      gate_hi = vm["gate_high"].as<double>();
+        if (vm.count("nvis"))           nvis = vm["nvis"].as<int>();
+        if (vm.count("nhid"))           nhid = vm["nhid"].as<int>();
+        if (vm.count("nout"))           nout = vm["nout"].as<int>();
+  	    if (vm.count("ip_v"))           ip_v  = vm["ip_v"].as<string>();
+  	    if (vm.count("ip_file"))        ipat_file  = vm["ip_file"].as<string>();
 
     }
     catch(std::exception& e) {
@@ -176,16 +176,16 @@ int main(int ac,char *av[]) {
 	SIFGroup * neurons_hid = new SIFGroup( nhid);
 
 	logger->msg("Done setting up neuron groups ...",PROGRESS,true);
-	logger->msg("Setting up Pattern Stimulator ...",PROGRESS,true);
-  PatternStimulator * stim = new PatternStimulator(neurons_vis, ip_v, ipat_file.c_str(), 1., nvis);
+    logger->msg("Setting up Pattern Stimulator ...",PROGRESS,true);
+    PatternStimulator * stim = new PatternStimulator(neurons_vis, ip_v, ipat_file.c_str(), 1., nvis);
 	logger->msg("Done setting up Pattern Stimulator ...",PROGRESS,true);
 
-  if(sigma>0){
-	PoissonStimulator * ps_hid = new PoissonStimulator( neurons_hid, 1000., sigma);
-	PoissonStimulator * ps_out = new PoissonStimulator( neurons_out, 1000., sigma);
-  } else {
-	logger->msg("No PoissonStimulator ...",PROGRESS,true);
-  }
+    if(sigma>0){
+        PoissonStimulator * ps_hid = new PoissonStimulator( neurons_hid, 1000., sigma);
+        PoissonStimulator * ps_out = new PoissonStimulator( neurons_out, 1000., sigma);
+    } else {
+        logger->msg("No PoissonStimulator ...",PROGRESS,true);
+    }
 	printf("prosyb %f", prob_syn);
 
 	neurons_out->set_refractory_period(3.9e-3);
@@ -212,7 +212,7 @@ int main(int ac,char *av[]) {
             gate_hi,
             AMPA);
 
-	peRBPConnection * con_ho 
+	peRBPConnection * con_ho
 		= new peRBPConnection(
             neurons_hid,
             neurons_out,
@@ -228,81 +228,79 @@ int main(int ac,char *av[]) {
 		= new SparseConnection( neurons_vis, neurons_err1,
             fwmat_ve.c_str(),
             MEM);
-  con_ve1->set_gain(1);
+    con_ve1->set_gain(1);
 
 	SparseConnection * con_ve2
 		= new SparseConnection( neurons_vis, neurons_err2,
             fwmat_ve.c_str(),
             MEM);
-  con_ve2->set_gain(-1);
+    con_ve2->set_gain(-1);
 
-	Connection * con_oe1 
+	Connection * con_oe1
 		= new SparseConnection( neurons_out, neurons_err1,
             fwmat_oe.c_str(),
             MEM);
-  con_oe1->set_gain(1);
+    con_oe1->set_gain(1);
 
-	Connection * con_oe2 
+	Connection * con_oe2
 		= new SparseConnection( neurons_out, neurons_err2,
             fwmat_oe.c_str(),
             MEM);
-  con_oe2->set_gain(-1);
+    con_oe2->set_gain(-1);
 
-  if (eta>0){
-	Connection * con_e1o 
-		= new SparseConnection( neurons_err1, neurons_out,
-            fwmat_eo.c_str(),
-            DEND);
-  con_e1o->set_gain(1);
+    if (eta>0){
+        Connection * con_e1o
+            = new SparseConnection( neurons_err1, neurons_out,
+                fwmat_eo.c_str(),
+                DEND);
+        con_e1o->set_gain(1);
 
-	Connection * con_e2o 
-		= new SparseConnection( neurons_err2, neurons_out,
-            fwmat_eo.c_str(),
-            DEND);
-  con_e2o->set_gain(-1);
+        Connection * con_e2o
+            = new SparseConnection( neurons_err2, neurons_out,
+                fwmat_eo.c_str(),
+                DEND);
+        con_e2o->set_gain(-1);
 
-	Connection * con_e1h 
-		= new SparseConnection( neurons_err1, neurons_hid,
-            fwmat_eh.c_str(),
-            DEND);
-  con_e1h->set_gain(1);
-
-
-	Connection * con_e2h 
-		= new SparseConnection( neurons_err2, neurons_hid,
-            fwmat_eh.c_str(),
-            DEND);
-  con_e2h->set_gain(-1);
-
-  }
-
-  if (!fwmat_hh.empty()){
-	peRBPConnection * con_hh
-		= new peRBPConnection(
-            neurons_hid,
-            neurons_hid,
-            fwmat_hh.c_str(),
-            prob_syn,
-            eta, //eta
-            -25,
-            25,
-            AMPA);
+        Connection * con_e1h
+            = new SparseConnection( neurons_err1, neurons_hid,
+                fwmat_eh.c_str(),
+                DEND);
+        con_e1h->set_gain(1);
 
 
-  }
+        Connection * con_e2h
+            = new SparseConnection( neurons_err2, neurons_hid,
+                fwmat_eh.c_str(),
+                DEND);
+        con_e2h->set_gain(-1);
 
-  std::stringstream filename;
-  if (record){
-	BinaryStateMonitor * dend_mon = new BinaryStateMonitor( neurons_out, 0, "dendrite", sys->fn("bdendrite") );
-	BinaryStateMonitor * mem_mon = new BinaryStateMonitor( neurons_out, 0, "mem", sys->fn("bmem") );
-	BinaryStateMonitor * vis_mem_mon = new BinaryStateMonitor( neurons_vis, 127, "bg_current", sys->fn("bmemvis") );
-	BinaryStateMonitor * g_ampa_mon = new BinaryStateMonitor( neurons_out, 0, "g_ampa", sys->fn("bampa") );
-	BinaryStateMonitor * errmon = new BinaryStateMonitor( neurons_err1, 0, "mem", sys->fn("mem") );
-  oss.str("");
-	oss << outputfile << "v.w";
-	WeightMonitor * smon = new WeightMonitor( con_ho, oss.str(), auryn_timestep);
-  smon->add_to_list(32,0);
-  }
+    }
+
+    if (!fwmat_hh.empty()){
+        peRBPConnection * con_hh
+            = new peRBPConnection(
+                neurons_hid,
+                neurons_hid,
+                fwmat_hh.c_str(),
+                prob_syn,
+                eta, //eta
+                -25,
+                25,
+                AMPA);
+    }
+
+    std::stringstream filename;
+    if (record){
+        BinaryStateMonitor * dend_mon = new BinaryStateMonitor( neurons_out, 0, "dendrite", sys->fn("bdendrite") );
+        BinaryStateMonitor * mem_mon = new BinaryStateMonitor( neurons_out, 0, "mem", sys->fn("bmem") );
+        BinaryStateMonitor * vis_mem_mon = new BinaryStateMonitor( neurons_vis, 127, "bg_current", sys->fn("bmemvis") );
+        BinaryStateMonitor * g_ampa_mon = new BinaryStateMonitor( neurons_out, 0, "g_ampa", sys->fn("bampa") );
+        BinaryStateMonitor * errmon = new BinaryStateMonitor( neurons_err1, 0, "mem", sys->fn("mem") );
+        oss.str("");
+        oss << outputfile << "v.w";
+        WeightMonitor * smon = new WeightMonitor( con_ho, oss.str(), auryn_timestep);
+        smon->add_to_list(32,0);
+    }
 
 	if ( record_rasters ) {
 		msg = "Setting up monitors ...";
@@ -326,12 +324,12 @@ int main(int ac,char *av[]) {
       = new SpikeMonitor( neurons_vis, filename.str().c_str() );
 	}
 
-  if ( record_rates ) {
+    if ( record_rates ) {
 		filename.str("");
 		filename.clear();
 		filename << outputfile << "out.ras";
 		SpikeMonitor * smon_out = new SpikeMonitor( neurons_out, filename.str().c_str() );
-  }
+    }
 
 
 
@@ -346,7 +344,7 @@ int main(int ac,char *av[]) {
 
 
 	logger->msg("Simulating ..." ,PROGRESS,true);
-	if (!sys->run(simtime,true)) 
+	if (!sys->run(simtime,true))
 			errcode = 1;
 
 	if ( sys->mpi_rank() == 0 ) {
@@ -360,7 +358,7 @@ int main(int ac,char *av[]) {
 	}
 
 	logger->msg("Saving network state ...",PROGRESS,true);
-  sys->save_network_state_text(outputfile);
+    sys->save_network_state_text(outputfile);
 	if (errcode)
 		auryn_abort(errcode);
 
--- src/auryn/peRBPConnection.cpp
+++ src/auryn/peRBPConnection.cpp
@@ -1,25 +1,25 @@
-/* 
+/*
 * Copyright 2014-2016 Friedemann Zenke
 *
 * This file is part of Auryn, a simulation package for plastic
 * spiking neural networks.
-* 
+*
 * Auryn is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
-* 
+*
 * Auryn is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
 * along with Auryn.  If not, see <http://www.gnu.org/licenses/>.
 *
 * If you are using Auryn or parts of it for your work please cite:
-* Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations 
-* of spiking neural networks using general-purpose computers. 
+* Zenke, F. and Gerstner, W., 2014. Limits to high-speed simulations
+* of spiking neural networks using general-purpose computers.
 * Front Neuroinform 8, 76. doi: 10.3389/fninf.2014.00076
 */
 
@@ -27,7 +27,7 @@
 
 using namespace auryn;
 
-void peRBPConnection::init(AurynDouble learning_rate, AurynDouble prob_syn) 
+void peRBPConnection::init(AurynDouble learning_rate, AurynDouble prob_syn)
 {
 	if ( dst->get_post_size() == 0 ) return;
 	// Synaptic traces
@@ -35,17 +35,17 @@ void peRBPConnection::init(AurynDouble learning_rate, AurynDouble prob_syn)
 	transmission_prob = prob_syn; // Stochastic blank_out probability
 }
 
-peRBPConnection::peRBPConnection(SpikingGroup * source, NeuronGroup * destination, 
-		const char * filename , 
+peRBPConnection::peRBPConnection(SpikingGroup * source, NeuronGroup * destination,
+		const char * filename ,
     AurynDouble prob_syn,
 		TransmitterType transmitter,
     std::string name
-    ) 
+    )
 : SparseConnection(source, destination, filename, transmitter)
 {
 	init(0., prob_syn);
-  modstate = dst->get_state_vector("mem"); 
-  vmodstate = dst->get_state_vector("mem"); 
+  modstate = dst->get_state_vector("mem");
+  vmodstate = dst->get_state_vector("mem");
   glow = 0.;
   ghigh = 0.;
   //set_name(name);
@@ -53,21 +53,21 @@ peRBPConnection::peRBPConnection(SpikingGroup * source, NeuronGroup * destinatio
 
 peRBPConnection::peRBPConnection(
     SpikingGroup * source,
-    NeuronGroup * destination, 
-		const char * filename , 
+    NeuronGroup * destination,
+		const char * filename ,
     AurynDouble prob_syn,
     AurynDouble learning_rate,
     AurynDouble gate_lo,
     AurynDouble gate_hi,
 		TransmitterType transmitter,
     std::string name
-    ) 
+    )
 : SparseConnection(source, destination, filename, transmitter)
 {
 	init(learning_rate, prob_syn);
   //set_name(name);
-  modstate = dst->get_state_vector("dendrite"); 
-  vmodstate = dst->get_state_vector("g_ampa"); 
+  modstate = dst->get_state_vector("dendrite");
+  vmodstate = dst->get_state_vector("g_ampa");
   glow = gate_lo;
   ghigh = gate_hi;
 }
@@ -83,13 +83,15 @@ void peRBPConnection::propagate_forward()
 			spike != src->get_spikes()->end() ; ++spike ) {
 		for (NeuronID * c = w->get_row_begin(*spike) ; c != w->get_row_end(*spike) ; ++c ) { // c = post index
       if ((*die)() < transmission_prob){
-			AurynWeight * weight = w->get_data_ptr(c);       
-			if ( stdp_active && (sys->get_clock()%2500)>500)  *weight += dw(*c);
-      transmit( *c , *weight );
+				AurynWeight * weight = w->get_data_ptr(c);
+				if ( stdp_active && (sys->get_clock()%2500)>500){
+					*weight += dw(*c);
+				}
+      	transmit( *c , *weight );
 
 			//NeuronID * ind = w->get_ind_begin(); // first element of index array
 			//AurynWeight * weight = w->get_data_begin();
-			//AurynWeight value = weight[c-ind]; 
+			//AurynWeight value = weight[c-ind];
 			//transmit( *c , value );
 			//if ( stdp_active && (sys->get_clock()%2500)>500)  weight[c-ind] += dw(*c);
 
@@ -100,7 +102,7 @@ void peRBPConnection::propagate_forward()
 
 AurynWeight peRBPConnection::dw(NeuronID post)
 {
-	NeuronID translated_spike = dst->global2rank(post); 
+	NeuronID translated_spike = dst->global2rank(post);
   if ( vmodstate->get(translated_spike)<ghigh)
     if ( vmodstate->get(translated_spike)>glow){
       return eta*modstate->get(translated_spike);
@@ -127,8 +129,3 @@ void peRBPConnection::set_eta(AurynFloat value)
     stdp_active=false;
   }
 }
-
-
-
-
-
